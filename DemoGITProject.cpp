b6216af485d39841aae18b5fe5837623821bdcf3
Paula Hannan <paula.hannan@oit.edu>
2019-09-14 12:26:05 -0700

    Add project files.

---
+++ DemoGITProject.cpp
@@ -0,0 +1,20 @@
+// DemoGITProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Koans are an interesting way to learn a programming language.  
+	cout << "C++ is not too hard to learn!" << endl
+		<< "Then go to the point of the first error." << endl
+		<< "And make the test pass." << endl
+		<< "We start right here:" << endl;
+
+	a;slfdj_gobldygookisnotc++
+
+	cout << "That was easy, wasn't it?" << endl
+		<< "Go on while you walk the path to enlightment ..." << endl;
+
+}
